6696452be10e6c90b1481b431115adf3c2b5239b
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-08-30 09:59:35 +0200

    created component hub and deleted class hub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HubComponent } from './hub/hub.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HubComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hub.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Hub } from './hub';
-
-describe('Hub', () => {
-  it('should create an instance', () => {
-    expect(new Hub()).toBeTruthy();
-  });
-});
