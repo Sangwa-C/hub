6b68649b7cbe4cf8b6a5889da8210a3bf5acb7fe
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-09-01 03:41:35 +0200

    last edites

--- src/app/d-counting.pipe.ts
+++ src/app/d-counting.pipe.ts
@@ -5,8 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DCountingPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  
+  transform(value: any): number {
+    let dayHour:Date =  new Date();
+    let day:any =  new Date(dayHour.getFullYear(), dayHour.getMonth(),dayHour.getDate());
+    var sub= Math.abs(value - day);
+    const SInD=86400;
+    var subSec=sub*0.001;
+    var results = subSec/SInD;
+    
+    if (results >= 1 && value > day){
+      return results
+    }
+    else{
+      return results;
+    }
   }
-
 }
+
+// export class BoldPipe implements PipeTransform {
+//   transform(value: string): string {
+//     if (value === null) return;
+//     return value.charAt(0).toUpperCase() + value.slice(1);
+//   }
+//   }
